359f6b30a643b9d1b374ae47475953808b53fbaa
Solohan <radekcz@gmail.com>
2022-08-17 23:05:51 +0200

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,7 +1,9 @@
 @echo off&setlocal
 :: start_kakadufm.bat
 
-set app=myapp.py
+set app=kakadufm.pyw
+set python_url=https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe
+set python_file=python-3.10.6-amd64.exe
 
 :: If venv exist - start app else run tests and make venv
 if exist venv\ (
