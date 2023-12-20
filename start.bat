80540c59bc2e6127036d0a4779dec3555abb76c7
Dobrowit <radekcz@gmail.com>
2023-12-20 21:46:48 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off&setlocal
 :: start.bat
 
-set app=app.py
+set app=example-app.py
 set python_url=https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe
 set python_file=python-3.10.6-amd64.exe
 
